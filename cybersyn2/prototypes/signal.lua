d2ae09d92b1751368ae8dedbce549f04de5d6846
William C. Johnson <wcjohnson@oigroup.net>
2025-03-12 07:14:52 -0700

    fix: Shorten virtual signal name

--- cybersyn2/prototypes/signal.lua
+++ cybersyn2/prototypes/signal.lua
@@ -2,12 +2,12 @@ signal_subgroup = {
 	type = "item-subgroup",
 	name = "cybersyn2-signals",
 	group = "signals",
-	order = "f"
+	order = "f",
 }
 
 group_signal = {
 	type = "virtual-signal",
-	name = "cybersyn2-group-signal",
+	name = "cybersyn2-group",
 	icon = "__cybersyn2__/graphics/icons/group-signal.png",
 	icon_size = 64,
 	subgroup = "cybersyn2-signals",
