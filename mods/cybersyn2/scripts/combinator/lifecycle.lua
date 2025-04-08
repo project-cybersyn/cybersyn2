fd98c6a3c9a00558b55de6360a275ce10bd1c6d4
William C. Johnson <wcjohnson@oigroup.net>
2025-04-07 21:43:28 -0700

    fix: Remove crosswiring, clear decider outputs on BP creation

--- mods/cybersyn2/scripts/combinator/lifecycle.lua
+++ mods/cybersyn2/scripts/combinator/lifecycle.lua
@@ -92,27 +92,6 @@ cs2.on_built_combinator(function(combinator_entity, tags)
 		outputs = {},
 	}
 
-	-- Crosswire i/o
-	local i_red = combinator_entity.get_wire_connector(
-		defines.wire_connector_id.combinator_input_red,
-		true
-	)
-	local o_red = combinator_entity.get_wire_connector(
-		defines.wire_connector_id.combinator_output_red,
-		true
-	)
-	i_red.connect_to(o_red, false, defines.wire_origin.script)
-
-	local i_green = combinator_entity.get_wire_connector(
-		defines.wire_connector_id.combinator_input_green,
-		true
-	)
-	local o_green = combinator_entity.get_wire_connector(
-		defines.wire_connector_id.combinator_output_green,
-		true
-	)
-	i_green.connect_to(o_green, false, defines.wire_origin.script)
-
 	cs2.raise_combinator_created(comb)
 end)
 
@@ -188,9 +167,29 @@ end)
 
 -- Extract settings tags on blueprint setup.
 cs2.on_blueprint_setup(function(event)
+	-- Save config tags into blueprint
 	bplib.save_tags(event, function(entity)
 		if entity_is_combinator_or_ghost(entity) then
 			return get_raw_settings(entity)
 		end
 	end)
+	-- Remove decider combinator outputs
+	local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
+	local blueprintish =
+		bplib.get_actual_blueprint(player, event.record, event.stack)
+	if not blueprintish then return end
+	local bp_entities = blueprintish.get_blueprint_entities()
+	if not bp_entities then return end
+	local changed = false
+	for _, entity in pairs(bp_entities) do
+		if entity.name == COMBINATOR_NAME then
+			if
+				entity.control_behavior and entity.control_behavior.decider_conditions
+			then
+				entity.control_behavior.decider_conditions.outputs = {}
+				changed = true
+			end
+		end
+	end
+	if changed then blueprintish.set_blueprint_entities(bp_entities) end
 end)
