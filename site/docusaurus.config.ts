e5de71afbb60f56aaeb31c84da82f3358f1ac21c
William C. Johnson <wcjohnson@oigroup.net>
2025-11-27 18:29:59 -0700

    docs: Remove docusaurus homepage

--- site/docusaurus.config.ts
+++ site/docusaurus.config.ts
@@ -41,6 +41,7 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/project-cybersyn/cybersyn2/tree/main/site/',
+          routeBasePath: '/',
         },
         theme: {
           customCss: './src/css/custom.css',
