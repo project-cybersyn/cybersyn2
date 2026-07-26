e1091c74e5a42bef95c6613587c29c8903f6477e
William C. Johnson <wcjohnson@oigroup.net>
2026-07-25 17:10:40 -0700

    perf: clean order read elision

--- mods/cybersyn2/scripts/logistics/order.lua
+++ mods/cybersyn2/scripts/logistics/order.lua
@@ -105,24 +105,44 @@ function Order:set_status(status, info)
 	self.status_tick = game.tick
 end
 
+function Order:clear_prov_req()
+	if next(self.provides) then self.provides = {} end
+	if next(self.requests) then self.requests = {} end
+	if next(self.requested_fluids) then self.requested_fluids = {} end
+end
+
 ---Read the value of this order from its known combinator
 ---@param workload Core.Thread.Workload|nil If given, the workload of this operation will be added to the counter.
 ---@return boolean updated `true` if the order was updated
 function Order:read(workload)
 	add_workload(workload, 1)
 
-	-- Early clear provides/reqs
-	if next(self.provides) then self.provides = {} end
-	if next(self.requests) then self.requests = {} end
-	if next(self.requested_fluids) then self.requested_fluids = {} end
-
-	-- Sanity checks
+	-- Early order read: done even if stop is not dirty
 	local stop = cs2.get_stop(self.node_id, true)
-	if not stop then return false end
+	if not stop then
+		self:clear_prov_req()
+		return false
+	end
 	local comb = cs2.get_combinator(self.combinator_id, true)
-	if not comb then return false end
+	if not comb then
+		self:clear_prov_req()
+		return false
+	end
 	local inventory = self.inventory
-	if not inventory then return false end
+	if not inventory then
+		self:clear_prov_req()
+		return false
+	end
+	-- This can change in realtime so we get it before is_dirty check.
+	self.busy_value = stop:get_occupancy()
+	add_workload(workload, 1)
+
+	-- Elide order reading for clean stops
+	if not stop:is_dirty() then return false end
+
+	-- Early clear provides/reqs
+	self:clear_prov_req()
+
 	local inputs = self.combinator_input == "green" and comb.green_inputs
 		or comb.red_inputs
 	local arity = self.arity
@@ -169,7 +189,6 @@ function Order:read(workload)
 	self.stacked_requests = stacked_requests
 	self.no_starvation = no_starvation
 	self.round_to_stacks = round_to_stacks
-	self.busy_value = stop:get_occupancy()
 	self.priority = stop.priority or 0
 	self.thresh_depletion_fraction = stop.auto_threshold_fraction
 	self.thresh_fullness_fraction = stop.train_fullness_fraction
@@ -181,7 +200,7 @@ function Order:read(workload)
 	local thresh_fullness_fluid = (stop_amfc or 0) * fullness_fraction
 
 	-- Workload for accumulating settings
-	add_workload(workload, 10)
+	add_workload(workload, 8)
 
 	-- Provides
 	local provides = self.provides
@@ -192,7 +211,7 @@ function Order:read(workload)
 			and not stop.is_consumer
 			and not comb:get_provide_subset()
 		then
-			local auto_provides = self.inventory.inventory or EMPTY
+			local auto_provides = inventory.inventory or EMPTY
 			assign(provides, auto_provides)
 			if workload then add_workload(workload, table_size(auto_provides)) end
 		end
