24b6379675688b6dcbe4420f89d3adfaac2cd544
William C. Johnson <wcjohnson@oigroup.net>
2026-07-28 03:32:02 -0700

    feat: added cs2 comb to research

--- mods/cybersyn2/prototypes/recipe.lua
+++ mods/cybersyn2/prototypes/recipe.lua
@@ -5,8 +5,11 @@ local combinator_recipe = data_lib.copy_prototype(
 	data.raw["recipe"]["decider-combinator"],
 	"cybersyn2-combinator"
 )
--- TODO: recipe enabled without research for testing, add tech for live
-combinator_recipe.enabled = true
 combinator_recipe.subgroup = data.raw["recipe"]["train-stop"].subgroup
 
 data:extend({ combinator_recipe })
+
+data_lib.unlock_recipe_with_technology(
+	"cybersyn2-combinator",
+	"automated-rail-transportation"
+)
